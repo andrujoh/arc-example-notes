0928d70e961a61a4b282a6cd42a039e3a22f8db2
Tor Andreas Johnsen <andru_joh@yahoo.no>
2023-04-12 04:12:17 +0200

    Prevent browser zoom on mobile

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -13,6 +13,9 @@ module.exports = function layout({ contents }) {
   return `<!DOCTYPE html>
 	<html>
 	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Architect demo app</title>
 		<link rel=stylesheet href="${static("/css/style.css")}">
 		<link rel="icon" type="image/png" sizes="16x16" href="${static("/images/architect-favicon-16.png")}">
